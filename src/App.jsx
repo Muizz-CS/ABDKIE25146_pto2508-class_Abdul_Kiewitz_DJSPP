cbe8cdb0109ddd227242ff76c1446eab1d572b54
Muizz-CS <kabdulmuizz@gmail.com>
2026-07-14 22:23:45 +0200

    add the player bar to app.jsx so it's on every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
+import AudioPlayerBar from "./components/layout/AudioPlayerBar";
 import LandingPage from "./pages/LandingPage";
 import ShowDetailPage from "./pages/ShowDetailPage";
 import FavouritesPage from "./pages/FavouritesPage";
@@ -8,13 +9,14 @@ export default function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="app-main">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/show/:id" element={<ShowDetailPage />} />
           <Route path="/favourites" element={<FavouritesPage />} />
         </Routes>
       </main>
+      <AudioPlayerBar />
     </>
   );
 }
