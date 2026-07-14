add4c4a6f2ff6629cd2a6e002e4a4ec70851715a
Muizz-CS <kabdulmuizz@gmail.com>
2026-07-14 16:29:53 +0200

    put the header above the routes so it shows on every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
 import LandingPage from "./pages/LandingPage";
 import ShowDetailPage from "./pages/ShowDetailPage";
 import FavouritesPage from "./pages/FavouritesPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/show/:id" element={<ShowDetailPage />} />
-      <Route path="/favourites" element={<FavouritesPage />} />
-    </Routes>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/show/:id" element={<ShowDetailPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Routes>
+      </main>
+    </>
   );
 }
