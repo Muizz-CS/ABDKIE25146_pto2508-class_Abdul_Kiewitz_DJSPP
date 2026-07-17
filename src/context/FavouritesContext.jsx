8cb80dc559e93bddc4b3bf293c39748eee36347e
Muizz-CS <kabdulmuizz@gmail.com>
2026-07-17 11:47:11 +0200

    build out real favourites logic in the context, add/remove/toggle with localstorage

--- src/context/FavouritesContext.jsx
+++ src/context/FavouritesContext.jsx
@@ -3,11 +3,44 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const FavouritesContext = createContext(null);
 
+function makeFavouriteId(showId, seasonTitle, episodeNumber) {
+  return `${showId}-${seasonTitle}-${episodeNumber}`;
+}
+
 export function FavouritesProvider({ children }) {
   const [favourites, setFavourites] = useLocalStorage("favourites", []);
 
+  function isFavourite(showId, seasonTitle, episodeNumber) {
+    const id = makeFavouriteId(showId, seasonTitle, episodeNumber);
+    return favourites.some((fav) => fav.id === id);
+  }
+
+  function addFavourite(episode) {
+    const id = makeFavouriteId(episode.showId, episode.seasonTitle, episode.episode);
+    setFavourites((prev) => {
+      if (prev.some((fav) => fav.id === id)) return prev;
+      return [...prev, { ...episode, id, addedAt: new Date().toISOString() }];
+    });
+  }
+
+  function removeFavourite(showId, seasonTitle, episodeNumber) {
+    const id = makeFavouriteId(showId, seasonTitle, episodeNumber);
+    setFavourites((prev) => prev.filter((fav) => fav.id !== id));
+  }
+
+  function toggleFavourite(episode) {
+    const id = makeFavouriteId(episode.showId, episode.seasonTitle, episode.episode);
+    if (favourites.some((fav) => fav.id === id)) {
+      removeFavourite(episode.showId, episode.seasonTitle, episode.episode);
+    } else {
+      addFavourite(episode);
+    }
+  }
+
   return (
-    <FavouritesContext.Provider value={{ favourites, setFavourites }}>
+    <FavouritesContext.Provider
+      value={{ favourites, isFavourite, addFavourite, removeFavourite, toggleFavourite }}
+    >
       {children}
     </FavouritesContext.Provider>
   );
