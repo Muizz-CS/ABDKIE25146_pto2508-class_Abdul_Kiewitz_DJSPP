bfa33ac34378466a3ac43a46db3c2b7958b245cc
Muizz-CS <kabdulmuizz@gmail.com>
2026-07-17 11:47:37 +0200

    add a date+time formatter for when favourites were added

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -6,3 +6,14 @@ export function formatDate(isoString) {
     day: "numeric",
   });
 }
+
+export function formatDateTime(isoString) {
+  if (!isoString) return "";
+  return new Date(isoString).toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  });
+}
